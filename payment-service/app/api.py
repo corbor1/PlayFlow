111f3f3d4a3013d45a893f2a2a460bdd247f0f30
corbor1 <150583836+corbor1@users.noreply.github.com>
2026-05-12 16:29:01 +0300

    Delete payment-service directory
    delete

--- payment-service/app/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, FastAPI
-from app.schemas import PaymentCreate, PaymentResponse
-
-router = APIRouter()
-@router.post("/register")
-def create_transaction():
-    return {"message": "User registered successfully!"}
